93f4cff54b1b5c38e14c3e56a3ff24852c6f55df
Edwin-TB <86167157+Edwin-TB@users.noreply.github.com>
2024-04-26 10:52:50 -0400

    Added plots for release and started data class

--- ModelClassUNITLESS.py
+++ ModelClassUNITLESS.py
@@ -13,10 +13,11 @@ import scipy.integrate as int
 import matplotlib.pyplot as plt
 from pint import UnitRegistry
 ureg = UnitRegistry()
+plt.rcParams['axes.grid'] = True
 
 class Model():
 
-    def __init__(self, initial_concentration, time_duration, time_step, source, mass_coeffs, areas, volume):
+    def __init__(self, initial_concentration, time_duration, source, mass_coeffs, areas, volume):
         """
         Args:
             time_i (float): initial time, typically 0
@@ -31,7 +32,6 @@ class Model():
         """
         self._initial_concentration = initial_concentration
         self._time_duration = time_duration
-        self._time_step = time_step
         self._source = source
         self._mass_coeffs = mass_coeffs
         self._areas = areas
@@ -43,8 +43,6 @@ class Model():
         return self._initial_concentration
     def get_time_duration(self):
         return self._time_duration
-    def get_time_step(self):
-        return self._time_step
     def get_source(self):
         return self._source
     def get_mass_coeffs(self):
@@ -57,24 +55,58 @@ class Model():
     ############ model-specific methods ############
 
     def source_func(self, current_time): # GOOD
-        if 0 <= current_time <= 43200 or 86400 <= current_time <= 129600:
+        seconds_per_hour = 3600
+        half_day = 12*seconds_per_hour
+        full_day = 24*seconds_per_hour
+        if 0 <= current_time <= half_day or full_day <= current_time <= full_day + half_day:
             return 1
         return 0
     
-    def loss_func(self, current_concentration): # GOOD
+    def top_release(self): # GOOD
         k = self.get_mass_coeffs()
         A = self.get_areas()
-        print(current_concentration)
-        return np.dot(k,A)*current_concentration
+        return k[0]*A[0]
+
+    def wall_release(self): # GOOD
+        k = self.get_mass_coeffs()
+        A = self.get_areas()
+        return k[1]*A[1]
         
     def model_function(self, current_concentration, current_time):
-        dcdt = (self.source_func(current_time)*self.get_source() - self.loss_func(current_concentration)) / self.get_volume()
+
+        S = self.source_func(current_time)*self.get_source()
+        L = (self.top_release() + self.wall_release())
+        c = current_concentration
+        V = self.get_volume()
+        dcdt = (S - L * c) / V
         return dcdt
+    
+class Data():
+    def __init__(self, raw_data, timing, background, run_number):
+        self._raw_data = raw_data
+        self._timing = timing
+        self._background = background
+        self._run_number = run_number
+
+    def get_raw_data(self):
+        return self._raw_data
+    def get_background(self):
+        return self._background
+    def get_timing(self):
+        return self._timing
+    def get_run_number(self):
+        return self._run_number
+
+    def remove_background(self):
+        return self.get_raw_data() - self.get_background()
+    
+    def give_points(self):
+        return np.sum(self.remove_background(), axis = 1)
     
 def integrate_concentration(model):
     t_initial = 0
-    t_final = (model.get_time_duration() + model.get_time_step())
-    t_step = model.get_time_step()
+    t_final = (model.get_time_duration() + 1)
+    t_step = 1
     t = np.arange(t_initial, t_final, t_step)
 
     c0 = model.get_initial_concentration()
@@ -96,20 +128,72 @@ if __name__ == '__main__':
     ex.
     model_1 = model_class(parameters)
     """
-    initial_concentration = 0 
-    time_step = 1 
-    experiment_duration = 60
+
+
+    initial_concentration = 0  
+    days = 7
+    hours_per_day = 24
+    seconds_per_hour = 3600
+    experiment_duration = days * hours_per_day * seconds_per_hour
     source = 3.65e5 
     mass_transfer_coeffs = [4.56e-7, 9.11e-8] # [top, wall]
     areas = [0.001380, 0.011618] # [top, wall]
     volume = 0.000100 
 
-    model = Model(initial_concentration, experiment_duration, time_step, source, mass_transfer_coeffs, areas, volume)
+    model = Model(initial_concentration, experiment_duration, source, mass_transfer_coeffs, areas, volume)
+    raw_data = np.array([[20, 0, 0, 0],
+                         [0, 0, 0, 0],
+                         [0, 0, 0, 0],
+                         [0, 0, 0, 0],
+                         [0, 0, 0, 0],
+                         [0, 0, 0, 0],
+                         [0, 0, 0, 0],
+                         [0, 0, 0, 0]])
+    timing = np.array([0, 1, 2, 3, 4, 5, 6, 7]) * hours_per_day * seconds_per_hour
+    background = 0
+    run_number = 7
+    data_points = Data(raw_data, timing, background, run_number)
+    
     concentration = integrate_concentration(model)[0]
     time = integrate_concentration(model)[1]
-    fig, ax = plt.subplots()
-    ax.plot(time, quantity_to_activity(concentration)*volume)
-    ax.grid()
+    top_release_values = model.top_release() * concentration
+    wall_release_values = model.wall_release() * concentration
+
+    cumulative_top_release = int.cumulative_trapezoid(np.asarray(top_release_values).squeeze(), time, initial=0)
+    cumulative_wall_release = int.cumulative_trapezoid(np.asarray(wall_release_values).squeeze(), time, initial=0)
+
+    activity = quantity_to_activity(concentration) * volume
+    top_release_activity = quantity_to_activity(top_release_values)
+    wall_release_activity = quantity_to_activity(wall_release_values)
+    cumulative_top_release_activity = quantity_to_activity(cumulative_top_release)
+    cumulative_wall_release_activity = quantity_to_activity(cumulative_wall_release)
+
+    x_axis_points = np.arange(0,experiment_duration+1, hours_per_day * seconds_per_hour)
+    x_axis_labels = np.arange(0,days+1,1)
+
+    data_x = data_points.get_timing()
+    data_y = data_points.give_points()
+
+    fig, axs = plt.subplots(3, sharex = True, figsize = (10,10))
+    axs[0].plot(time, activity)
+    axs[0].set_title("Salt tritium inventory V*c_salt")
+    axs[0].set_ylabel("Bq")
+    axs[1].plot(time, top_release_activity)
+    axs[1].plot(time, wall_release_activity)
+    axs[1].set_title("Tritium Release Rate")
+    axs[1].set_ylabel("Bq/s")
+    axs[2].plot(time, cumulative_top_release_activity)
+    axs[2].plot(time, cumulative_wall_release_activity)
+    axs[2].plot(data_x, data_y,'ro')
+    axs[2].set_xlabel("Days")
+    axs[2].set_title("Cumulative Tritium Release")
+    axs[2].set_ylabel("Bq")
+    for ax in axs:
+        ax.axvspan(0, 12*seconds_per_hour, alpha = 0.2, color = 'red')
+        ax.axvspan(24*seconds_per_hour, 36*seconds_per_hour, alpha = 0.2, color = 'red')
+
+
+    plt.xticks(x_axis_points,x_axis_labels)
     plt.show()
 
 
